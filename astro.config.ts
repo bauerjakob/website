f57165c0bbfa092737b5d63ffa981cd6d867a7ce
Jakob <mail@bauer-jakob.de>
2024-03-29 19:55:26 +0100

    Fixed mobile scaling problem and don't deploy to github pages

--- astro.config.ts
+++ astro.config.ts
@@ -10,34 +10,38 @@ import image from "@astrojs/image";
 // import node from "@astrojs/node";
 
 // https://astro.build/config
-export default defineConfig({
-    site: "https://bauerjakob.github.io",
-    base: "/website",
-    markdown: {
-        shikiConfig: {
-            theme: "dracula",
-            wrap: true
-        }
-    },
-    integrations: [sitemap(), image({
-        serviceEntryPoint: "@astrojs/image/sharp"
-    }), compress({
-        css: false,
-        html: {
-            removeComments: true,
-            removeAttributeQuotes: false
-        },
-        img: false,
-        js: false,
-        svg: true
-    }), prefetch()],
-    vite: {
-        build: {
-            assetsInlineLimit: 0
-        }
-    },
-    // output: "server",
-    // adapter: node({
-    //     mode: 'standalone',
-    // }),
-});
+
+export default defineConfig({});
+
+// Config for Github Pages
+// export default defineConfig({
+//     site: "https://bauerjakob.github.io",
+//     base: "/website",
+//     markdown: {
+//         shikiConfig: {
+//             theme: "dracula",
+//             wrap: true
+//         }
+//     },
+//     integrations: [sitemap(), image({
+//         serviceEntryPoint: "@astrojs/image/sharp"
+//     }), compress({
+//         css: false,
+//         html: {
+//             removeComments: true,
+//             removeAttributeQuotes: false
+//         },
+//         img: false,
+//         js: false,
+//         svg: true
+//     }), prefetch()],
+//     vite: {
+//         build: {
+//             assetsInlineLimit: 0
+//         }
+//     },
+//     // output: "server",
+//     // adapter: node({
+//     //     mode: 'standalone',
+//     // }),
+// });
